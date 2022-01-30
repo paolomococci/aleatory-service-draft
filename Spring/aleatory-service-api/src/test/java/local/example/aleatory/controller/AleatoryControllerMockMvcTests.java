d99c5657229bcb3dfb27431efae575dbac9400b6
paolomococci <paolomococci@gmail.com>
2022-01-30 19:23:22 +0100

    edited class AleatoryControllerMockMvcTests belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/test/java/local/example/aleatory/controller/AleatoryControllerMockMvcTests.java
+++ Spring/aleatory-service-api/src/test/java/local/example/aleatory/controller/AleatoryControllerMockMvcTests.java
@@ -1,10 +1,20 @@
 package local.example.aleatory.controller;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 public class AleatoryControllerMockMvcTests {
+
+    private static final String PATH = "/";
+    private MockMvc mockMvc;
+
+    @Autowired
+    WebApplicationContext webAppCtx;
 
     @Test
     void sampleTest() {
