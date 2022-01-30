15093601129b8eaade89eb603f80389ed18c3e57
paolomococci <paolomococci@gmail.com>
2022-01-30 07:10:17 +0100

    added class AleatoryResource belonging to project aleatory-service-api

---
+++ Quarkus/aleatory-service-api/src/main/java/local/example/aleatory/AleatoryResource.java
@@ -0,0 +1,16 @@
+package local.example.aleatory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/aleatory")
+public class AleatoryResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String feedback() {
+        return "-- feedback from aleatory service API ---";
+    }
+}
