19f9c12ddf63ec3a964aed50704214f2c45d4948
paolomococci <paolomococci@gmail.com>
2022-01-30 08:46:37 +0100

    added class ApplicationTests belonging to project aleatory-service-api

---
+++ Spring/aleatory-service-api/src/test/java/local/example/aleatory/ApplicationTests.java
@@ -0,0 +1,13 @@
+package local.example.aleatory;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void sampleTest() {
+	}
+}
