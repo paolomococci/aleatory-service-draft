9ca660a94b5dbf956798c9532c8203d0ce0fb34b
paolomococci <paolomococci@gmail.com>
2022-01-30 19:22:03 +0100

    edited class AleatoryRawControllerTests belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/test/java/local/example/aleatory/controller/AleatoryRawControllerTests.java
+++ Spring/aleatory-service-api/src/test/java/local/example/aleatory/controller/AleatoryRawControllerTests.java
@@ -1,10 +1,15 @@
 package local.example.aleatory.controller;
 
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class AleatoryRawControllerTests {
+
+    @Autowired
+    private AleatoryRawController aleatoryRawController;
 
     @Test
     void sampleTest() {
