58594cc1d320929f9b5f15e101b423b817f26287
paolomococci <paolomococci@gmail.com>
2022-01-30 21:29:59 +0100

    edited class AleatoryRawController belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/main/java/local/example/aleatory/controller/AleatoryRawController.java
+++ Spring/aleatory-service-api/src/main/java/local/example/aleatory/controller/AleatoryRawController.java
@@ -1,7 +1,20 @@
 package local.example.aleatory.controller;
 
+import local.example.aleatory.service.AleatoryService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AleatoryRawController {
+
+    @RequestMapping(
+            value = "/raw",
+            method = RequestMethod.GET
+    )
+    public double rawResponse() {
+        AleatoryService aleatoryService = new AleatoryService();
+        return aleatoryService.getUniformRandomProvider();
+    }
 }
