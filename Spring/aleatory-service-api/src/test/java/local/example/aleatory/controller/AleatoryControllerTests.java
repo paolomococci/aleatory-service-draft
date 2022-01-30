ec1b2f2533d74bfe8558afcc0df9e6d2eba1a9d6
paolomococci <paolomococci@gmail.com>
2022-01-30 19:34:52 +0100

    edited class AleatoryControllerTests belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/test/java/local/example/aleatory/controller/AleatoryControllerTests.java
+++ Spring/aleatory-service-api/src/test/java/local/example/aleatory/controller/AleatoryControllerTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class AleatoryControllerTests {
 
@@ -12,6 +14,7 @@ public class AleatoryControllerTests {
     private AleatoryController aleatoryController;
 
     @Test
-    void sampleTest() {
+    public void notNullResponseTest() {
+        assertThat(aleatoryController).isNotNull();
     }
 }
