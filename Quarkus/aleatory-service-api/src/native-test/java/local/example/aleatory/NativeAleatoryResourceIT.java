5ecf6d665d201b1e3acc8220a7889edf51576d89
paolomococci <paolomococci@gmail.com>
2022-01-30 07:10:44 +0100

    added class NativeAleatoryResourceIT belonging to project aleatory-service-api

---
+++ Quarkus/aleatory-service-api/src/native-test/java/local/example/aleatory/NativeAleatoryResourceIT.java
@@ -0,0 +1,8 @@
+package local.example.aleatory;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeAleatoryResourceIT
+        extends AleatoryResourceTest {
+}
