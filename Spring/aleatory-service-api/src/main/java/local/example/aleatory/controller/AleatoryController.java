2db171212dcc2846ddd509b01d5bc801ab0714bc
paolomococci <paolomococci@gmail.com>
2022-01-30 21:30:07 +0100

    edited class AleatoryController belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/main/java/local/example/aleatory/controller/AleatoryController.java
+++ Spring/aleatory-service-api/src/main/java/local/example/aleatory/controller/AleatoryController.java
@@ -1,7 +1,27 @@
 package local.example.aleatory.controller;
 
+import java.security.SecureRandom;
+
+import local.example.aleatory.service.AleatoryService;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AleatoryController {
+
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.GET
+    )
+    public double defaultResponse() {
+        int index;
+        double[] temp;
+        SecureRandom secureRandom = new SecureRandom();
+        AleatoryService aleatoryService = new AleatoryService();
+        temp = aleatoryService.getUncorrelatedRandomVector();
+        index = (int) (256 * secureRandom.nextDouble());
+        return temp[index];
+    }
 }
