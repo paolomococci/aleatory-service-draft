88b7f9dfcab6e5f63014848a9fd6d82c0d3a3f30
paolomococci <paolomococci@gmail.com>
2022-01-30 21:22:59 +0100

    edited class AleatoryService belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
+++ Spring/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
@@ -1,4 +1,36 @@
 package local.example.aleatory.service;
 
+import org.apache.commons.math4.random.GaussianRandomGenerator;
+import org.apache.commons.math4.random.UncorrelatedRandomVectorGenerator;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class AleatoryService {
+
+    private final UniformRandomProvider uniformRandomProvider;
+    private final GaussianRandomGenerator gaussianRandomGenerator;
+    private final UncorrelatedRandomVectorGenerator uncorrelatedRandomVectorGenerator;
+
+    public AleatoryService() {
+        super();
+        int[] seed = RandomSource.createIntArray(256);
+        this.uniformRandomProvider = RandomSource.create(RandomSource.MT, seed);
+        this.gaussianRandomGenerator = new GaussianRandomGenerator(uniformRandomProvider);
+        this.uncorrelatedRandomVectorGenerator = new UncorrelatedRandomVectorGenerator(256, gaussianRandomGenerator);
+    }
+
+    public double getUniformRandomProvider() {
+        return uniformRandomProvider.nextDouble();
+    }
+
+    public double getGaussianRandomGenerator() {
+        return gaussianRandomGenerator.nextNormalizedDouble();
+    }
+
+    public double[] getUncorrelatedRandomVector() {
+        return uncorrelatedRandomVectorGenerator.nextVector();
+    }
 }
