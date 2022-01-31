bcc0a832c3bc9241ef649ee644eeafc023382d9d
paolomococci <paolomococci@gmail.com>
2022-01-31 09:37:11 +0100

    edited class AleatoryResourceMockMvcTest belonging to project aleatory-service-api

--- Quarkus/aleatory-service-api/src/test/java/local/example/aleatory/AleatoryResourceMockMvcTest.java
+++ Quarkus/aleatory-service-api/src/test/java/local/example/aleatory/AleatoryResourceMockMvcTest.java
@@ -2,8 +2,15 @@ package local.example.aleatory;
 
 import io.quarkus.test.junit.QuarkusTest;
 
+import org.junit.jupiter.api.BeforeAll;
+
 @QuarkusTest
 public class AleatoryResourceMockMvcTest {
 
     private static final String PATH = "/aleatory";
+
+    @BeforeAll
+    public static void setUp() {
+
+    }
 }
