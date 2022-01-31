0b75fc2d16b25184d24794f863676c8ecf5b6f76
paolomococci <paolomococci@gmail.com>
2022-01-31 08:34:15 +0100

    edited class AleatoryService belonging to project aleatory-service-api

--- Spring/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
+++ Spring/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
@@ -1,36 +1,26 @@
 package local.example.aleatory.service;
 
-import org.apache.commons.math3.random.GaussianRandomGenerator;
-import org.apache.commons.math3.random.RandomGenerator;
-import org.apache.commons.math3.random.UncorrelatedRandomVectorGenerator;
+import org.apache.commons.math3.random.*;
 import org.apache.commons.rng.UniformRandomProvider;
 import org.apache.commons.rng.simple.RandomSource;
 
 public class AleatoryService {
 
     private final UniformRandomProvider uniformRandomProvider;
-    private final GaussianRandomGenerator gaussianRandomGenerator;
     private final UncorrelatedRandomVectorGenerator uncorrelatedRandomVectorGenerator;
 
     public AleatoryService() {
-        super();
-        int[] seed = RandomSource.createIntArray(256);
-        // TODO, to solve: deprecated
-        this.uniformRandomProvider = RandomSource.create(RandomSource.MT, seed);
-        // TODO, to solve: cannot be cast
-        this.gaussianRandomGenerator = new GaussianRandomGenerator((RandomGenerator) uniformRandomProvider);
-        this.uncorrelatedRandomVectorGenerator = new UncorrelatedRandomVectorGenerator(
+        uniformRandomProvider = RandomSource.create(RandomSource.WELL_512_A);
+        uncorrelatedRandomVectorGenerator = new UncorrelatedRandomVectorGenerator(
                 256,
-                gaussianRandomGenerator
+                new GaussianRandomGenerator(
+                        new ISAACRandom()
+                )
         );
     }
 
     public double getUniformRandomProvider() {
         return uniformRandomProvider.nextDouble();
-    }
-
-    public double getGaussianRandomGenerator() {
-        return gaussianRandomGenerator.nextNormalizedDouble();
     }
 
     public double[] getUncorrelatedRandomVector() {
