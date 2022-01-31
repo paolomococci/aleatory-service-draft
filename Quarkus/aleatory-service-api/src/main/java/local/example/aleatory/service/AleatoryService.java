569ccda6cc8630131a18861dfb1d7350e3ec7a55
paolomococci <paolomococci@gmail.com>
2022-01-31 08:55:47 +0100

    edited class AleatoryService belonging to project aleatory-service-api

--- Quarkus/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
+++ Quarkus/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
@@ -1,4 +1,29 @@
 package local.example.aleatory.service;
 
+import org.apache.commons.math3.random.*;
+import org.apache.commons.rng.UniformRandomProvider;
+import org.apache.commons.rng.simple.RandomSource;
+
 public class AleatoryService {
+
+    private final UniformRandomProvider uniformRandomProvider;
+    private final UncorrelatedRandomVectorGenerator uncorrelatedRandomVectorGenerator;
+
+    public AleatoryService() {
+        uniformRandomProvider = RandomSource.create(RandomSource.WELL_512_A);
+        uncorrelatedRandomVectorGenerator = new UncorrelatedRandomVectorGenerator(
+                256,
+                new GaussianRandomGenerator(
+                        new ISAACRandom()
+                )
+        );
+    }
+
+    public double getUniformRandomProvider() {
+        return uniformRandomProvider.nextDouble();
+    }
+
+    public double[] getUncorrelatedRandomVector() {
+        return uncorrelatedRandomVectorGenerator.nextVector();
+    }
 }
