d232aa1b68f617dcfd9f28cdbdd1adb490e56a99
paolomococci <paolomococci@gmail.com>
2022-02-05 16:26:17 +0100

    edited class AleatoryService belonging to project aleatory-service-api

--- Quarkus/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
+++ Quarkus/aleatory-service-api/src/main/java/local/example/aleatory/service/AleatoryService.java
@@ -4,12 +4,28 @@ import org.apache.commons.math3.random.*;
 import org.apache.commons.rng.UniformRandomProvider;
 import org.apache.commons.rng.simple.RandomSource;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
 public class AleatoryService {
 
     private final UniformRandomProvider uniformRandomProvider;
     private final UncorrelatedRandomVectorGenerator uncorrelatedRandomVectorGenerator;
 
+    private final RandomGenerator randomGenerator;
+    private final RandomGenerator.SplittableGenerator splittableGenerator;
+
     public AleatoryService() {
+        randomGenerator = RandomGenerator.of("L64X256MixRandom");
+
+        splittableGenerator = RandomGeneratorFactory
+                .<RandomGenerator.SplittableGenerator>of("L64X256MixRandom").create();
+
         uniformRandomProvider = RandomSource.create(RandomSource.WELL_512_A);
         uncorrelatedRandomVectorGenerator = new UncorrelatedRandomVectorGenerator(
                 256,
@@ -25,5 +41,27 @@ public class AleatoryService {
 
     public double[] getUncorrelatedRandomVector() {
         return uncorrelatedRandomVectorGenerator.nextVector();
+    }
+
+    public List<Long> getThreadSafeRandomLongNumberVector() {
+        List<Long> longs = Collections.synchronizedList(new ArrayList<>());
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        splittableGenerator.splits(256).forEach(
+                splittableGenerator -> executorService.submit(() -> {
+                    longs.add(splittableGenerator.nextLong());
+        }));
+        return longs;
+    }
+
+    public List<Double> getThreadSafeRandomDoubleNumberVector() {
+        List<Double> doubles = Collections.synchronizedList(new ArrayList<>());
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        splittableGenerator.splits(256).forEach(
+                splittableGenerator -> executorService.submit(() -> {
+                    doubles.add(splittableGenerator.nextDouble());
+        }));
+        return doubles;
     }
 }
