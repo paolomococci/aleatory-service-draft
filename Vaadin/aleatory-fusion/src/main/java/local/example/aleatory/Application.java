ff1225ddb11545675ff62dce062afe528a206cf5
paolomococci <paolomococci@gmail.com>
2022-02-08 17:45:54 +0100

    edited class Application belonging to project aleatory-fusion

--- Vaadin/aleatory-fusion/src/main/java/local/example/aleatory/Application.java
+++ Vaadin/aleatory-fusion/src/main/java/local/example/aleatory/Application.java
@@ -4,6 +4,7 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,10 +12,11 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 @Theme(value = "aleatory", variant = Lumo.DARK)
 @PWA(name = "Aleatory", shortName = "Aleatory", offlineResources = {"images/logo.png"})
-public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+public class Application
+        extends SpringBootServletInitializer
+        implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
